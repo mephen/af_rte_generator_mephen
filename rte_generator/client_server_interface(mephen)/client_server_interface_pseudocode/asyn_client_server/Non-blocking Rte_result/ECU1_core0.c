0675e85d5d2f6c76784faac479e779958dc54243
Mephen <boy4561400@gmail.com>
2024-03-19 01:25:50 +0800

    modify part of  C/S communication n:1 remix case pseudocode(async rte_call + non_blocking rte_result)

--- rte_generator/client_server_interface(mephen)/client_server_interface_pseudocode/asyn_client_server/Non-blocking Rte_result/ECU1_core0.c
+++ rte_generator/client_server_interface(mephen)/client_server_interface_pseudocode/asyn_client_server/Non-blocking Rte_result/ECU1_core0.c
@@ -1,54 +1,81 @@
-//Async Rte_call + non-blokcing Rte_result。
-    //transaction handle：用來做 response to request mapping。
-    //Rte_event：trigger client response runnable。
+void com_server_side();
 
-T01{//假設 client runnables 都配置在 T01
-    C/S request_ioc;
-        enqueue ioc transaction_handle_queue;
-    C/S request_ioc;
-        enqueue ioc transaction_handle_queue;
-
-    C/S request_rte;
-        enqueue rte transaction_handle_queue;
-    C/S request_rte;
-        enqueue rte transaction_handle_queue;
+void rte_callback(){
+    signal rte_event_operation_invoked_id = -1;
+    com_receiveSignal(signal_1, &rte_event_operation_invoked_id);
+    switch(rte_event_operation_invoked_id){
+        case 5:
+            rte_event_operation_invoked_5++;
+            break;
+        case 6:
+            rte_event_operation_invoked_6++;
+            break;
+    }
+    activateTask(T01);
+    return;
 }
 
-T02{//rte case: server-side
-    while(rte transaction_handle_queue not empty){
-        dequeue rte transaction_handle_queue;
-        switch(transaction_handle){
-            case a:
-                rte_server_side_1();//call transformer, check error, do server operation, and then enqueue result
-                Rte_event_1 = true;
-                break;
-            case b:
-                rte_server_side_2();
-                Rte_event_2 = true;
-                break;
+isr{//com 收到資料後，觸發 isr
+    rte_callback();
+}
+
+T01{//com server side: 接收request，處理request，發送response
+    while(operation_invoked_5){
+        com_server_side();//check error, call transformer, enqueue request queue, do operation, send response
+
+        getLock(lock_5, lock_bit);
+        rte_event_operation_invoked_5--;
+        releaseLock(lock_5, lock_bit);
+        
+        signal rte_event_async_return_id = 5;
+        com_sendSignal(signal_2, &rte_event_async_return_id);
+    }
+    while(operation_invoked_6){
+        com_server_side();//check error, call transformer, enqueue request queue, do operation, send response
+
+        getLock(lock_6, lock_bit);
+        rte_event_operation_invoked_6--;
+        releaseLock(lock_6, lock_bit);
+        
+        signal rte_event_async_return_id = 6;
+        com_sendSignal(signal_2, &rte_event_async_return_id);
+    }
+}
+
+void com_server_side(){
+    com_receiveSignal(signal_3, &data);//接收 parameters, transaction_handle, and operation string(e.g. "add", "sub")
+    if(data.operation_string == "add"){
+        check_transformer_buffer();
+        error = transformer(&transformed_data_1, data.parameters[0]);
+        check_transformer_hard_soft_error();
+
+        if(error == e_ok){
+            rte_enqueue(&transformed_data_1, request_queue);//在 server side enqueue request queue
+
+            response = server_runnable_1();//do "add" operation, add operation 會自己去 request_queue 裡面拿參數。
+            com_sendSignal(signal_4, &data.transaction_handle);
+            com_sendSignal(signal_5, &response);
         }
     }
-}
+    if(data.operation_string == "sub"){
+        std_return error_1, error_2;
+        
+        check_transformer_buffer();
+        error_1 = transformer(&transformed_data_1, data.parameters[0]);
+        check_transformer_hard_soft_error();
 
-T03{//假設 client response runnables 都配置在 T03
-    while(there is any Rte event be set){//use a global variable to record the number of set Rte_events
-        if(Rte_event_1){ rte_client_side_1(); response_runnable_1(); set Rte_events number--;}
-        if(Rte_event_2){ rte_client_side_2(); response_runnable_1(); set Rte_events number--;}
-        if(Rte_event_3){ ioc_client_side_1(); response_runnable_1(); set Rte_events number--;}
-        if(Rte_event_4){ ioc_client_side_2(); response_runnable_1(); set Rte_events number--;}
+        check_transformer_buffer();
+        error_2 = transformer(&transformed_data_2, data.parameters[1]);
+        check_transformer_hard_soft_error();
+        
+        if(error_1 == e_ok && error_2 == e_ok){
+            rte_enqueue(&transformed_data_1, request_queue);
+            rte_enqueue(&transformed_data_2, request_queue);
+
+            response = server_runnable_2();//do "sub" operation, sub operation 會自己去 request_queue 裡面拿參數。
+            com_sendSignal(signal_4, &data.transaction_handle);
+            com_sendSignal(signal_5, &response);
+        }
     }
-}
+};
 
-
-/*若 client response runnable 寫在不同 task 的情況*/
-T03{
-    while(!Rte_event_1){;}
-    rte_client_side_1(); response_runnable_1(); set Rte_events number--;
-}
-
-T04{
-    while(!Rte_event_2){;}
-    rte_client_side_2(); response_runnable_2(); set Rte_events number--;
-}
-
-//com case 還不確定他們 function 具體怎麼使用，所以暫時不寫。
